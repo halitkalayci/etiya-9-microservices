4be5ac75472790b0b9cb533b8f1953bdefc0e1a4
Halit Enes Kalaycı <halitenes.kalayci@gmail.com>
2024-10-03 11:24:05 +0300

    init

---
+++ productservice/src/test/java/com/etiya/productservice/ProductserviceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.etiya.productservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProductserviceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}

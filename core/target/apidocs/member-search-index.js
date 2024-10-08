c41a53defa6dc3a8a818b014bf84e7fd1c980d4b
Halit Enes Kalaycı <halitenes.kalayci@gmail.com>
2024-10-08 13:26:50 +0300

    plugin

---
+++ core/target/apidocs/member-search-index.js
@@ -0,0 +1,1 @@
+memberSearchIndex = [{"p":"io.github.halitkalayci","c":"App","l":"App()","u":"%3Cinit%3E()"},{"p":"io.github.halitkalayci","c":"App","l":"main(String[])","u":"main(java.lang.String[])"}];updateSearchResults();
